8b75bfa6e5aeec839bbd7f97f916fc0e07f97ece
Bcacao <126742424+Bcacao@users.noreply.github.com>
2023-04-01 21:34:44 +0900

    디자인 첫번째

--- js/end.js
+++ js/end.js
@@ -4,14 +4,24 @@ const $day = document.querySelector('.date p');
 const $seat = document.querySelector('.seat p');
 const $headCount = document.querySelector('.head-count p');
 const $qrcode = document.querySelector('.qrcode-img');
+const $secretBox = document.querySelectorAll('.secret-box li');
+const $secretBoxLi = [...$secretBox];
 
 
+// console.log($secretBoxLi[1]);
+// $age.innerHTML = '나이제한';
+// $movieTitle.innerHTML = '제목<br><span>서브타이틀</span>';
+// $day.innerHTML = 'cinemabox 날짜<br><span>시간</span>';
+// $seat.innerHTML = '스크린타입<br><span>좌석</span>';
+// $headCount.innerHTML = '인원';
+// $qrcode.src = '../image/소울메이트.png';
 
-console.log($qrcode);
-$age.innerHTML = '나이제한';
-$movieTitle.innerHTML = '제목<br><span>서브타이틀</span>';
-$day.innerHTML = 'cinemabox 날짜<br><span>시간</span>';
-$seat.innerHTML = '스크린타입<br><span>좌석</span>';
-$headCount.innerHTML = '인원';
-$qrcode.src = '../image/소울메이트.png';
 
+// $secretBoxLi[3].style.opacity= '0.1';
+// $secretBoxLi[3].style.transition = 'opacity 2s ease-out';
+
+ for(let i = 0; i < $secretBoxLi.length; i++){
+     $secretBoxLi[i].style.opacity= '0';
+     $secretBoxLi[i].style.transition = 'opacity '+ i +'s ease-out';
+     $secretBoxLi[i].style.transitionDelay = i-0.9+'s';
+ }
